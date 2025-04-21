3cb78edd9b9c7123d39ce66e1160d82f48e063c4
SunXP <xpsun@qq.com>
2025-04-21 20:38:58 +0800

    my first commit

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import '@unocss/reset/tailwind.css'
+import 'virtual:uno.css'
+
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
